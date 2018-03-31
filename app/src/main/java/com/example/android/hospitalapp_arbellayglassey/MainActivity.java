84e25310e281d1ae85dc8a75805b7cd4f60056b8
Idylx <idyllix9@gmail.com>
2018-03-31 15:20:57 +0200

    Test2

--- app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MainActivity.java
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+        //Un autre test d'amour
     }
 }
