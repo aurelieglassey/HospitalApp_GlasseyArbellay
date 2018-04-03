7ba13667f781775eeb2653357f9e81f07d823f9b
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-04-03 14:38:39 +0200

    Ajout des activity

---
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MedecineAdd.java
@@ -0,0 +1,13 @@
+package com.example.android.hospitalapp_arbellayglassey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedecineAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medecine_add);
+    }
+}
