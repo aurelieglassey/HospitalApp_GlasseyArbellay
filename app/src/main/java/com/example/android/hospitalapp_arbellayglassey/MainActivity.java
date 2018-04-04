35b7ab009b888ae83f102348350c0e4679c42cf7
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-04-04 11:43:44 +0200

    Activity add medecine search fini

--- app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MainActivity.java
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         btntest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MedecineAdd.class);
+                Intent intent = new Intent(MainActivity.this, MedecineAddSearchList.class);
                 MainActivity.this.startActivity(intent);
             }
         });
--- app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MedecineAddSearchList.java
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/MedecineAddSearchList.java
@@ -2,6 +2,12 @@ package com.example.android.hospitalapp_arbellayglassey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MedecineAddSearchList extends AppCompatActivity {
 
@@ -9,5 +15,42 @@ public class MedecineAddSearchList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medecine_add_search_list);
+
+
+        final String [] medecineAddSearchList = getResources().getStringArray(R.array.medecine_array);
+        ListView list;
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listmedecineaddsearchlist_layout, medecineAddSearchList) {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view;
+                //If View doesn't exist create a new view
+                if (convertView == null) {
+                    // Create the Layout
+                    LayoutInflater inflater = getLayoutInflater();
+                    view = inflater.inflate(R.layout.listmedecineaddsearchlist_layout, parent, false);
+                } else {
+                    view = convertView;
+                }
+
+                //Add Text to the layout
+                TextView textView1 = (TextView) view.findViewById(R.id.listview_listofmedecinefortreatment);
+                textView1.setText(medecineAddSearchList[position]);
+
+                return view;
+            }
+        };
+
+        list = (ListView) findViewById(R.id.listofmedecinesearchlist);
+        list.setAdapter(adapter);
+
+
+
+
+
+
+
+
     }
 }
