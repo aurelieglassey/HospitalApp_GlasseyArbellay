a869f7e4e92e0785e254d984105b2c5ff215b376
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-04-04 14:08:19 +0200

    Liaisons premiers bouttons

--- app/src/main/java/com/example/android/hospitalapp_arbellayglassey/ListOfPatientActivity.java
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/ListOfPatientActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.hospitalapp_arbellayglassey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,10 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class ListOfPatientActivity extends AppCompatActivity {
+
+    //Button add a new patient
+    private Button btnNewPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,17 @@ public class ListOfPatientActivity extends AppCompatActivity {
         final String [] patient = getResources().getStringArray(R.array.patient_array);
         ListView list;
 
+        btnNewPatient = (Button) findViewById(R.id.btn_add_patient);
+        btnNewPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListOfPatientActivity.this, PatientAdd.class);
+                ListOfPatientActivity.this.startActivity(intent);
+            }
+        });
+
         ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, R.layout.listofpatient_laylout, patient) {
+
 
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
@@ -46,7 +61,6 @@ public class ListOfPatientActivity extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.listofpatient);
         list.setAdapter(adapter);
-
 
 
 
