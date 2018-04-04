5ebda2d0d9849b7d09878cc35403aa45517b386c
Idylx <idyllix9@gmail.com>
2018-04-04 10:20:10 +0200

    begin treatment activity

---
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/TreatmentDetails.java
@@ -0,0 +1,13 @@
+package com.example.android.hospitalapp_arbellayglassey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TreatmentDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_treatment_details);
+    }
+}
