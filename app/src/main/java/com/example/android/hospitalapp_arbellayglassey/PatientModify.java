ae81d2fb395d881899f59ba5bc05ad86177eef8c
Idylx <idyllix9@gmail.com>
2018-04-14 10:52:38 +0200

    Add some button

--- app/src/main/java/com/example/android/hospitalapp_arbellayglassey/PatientModify.java
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/PatientModify.java
@@ -1,13 +1,41 @@
 package com.example.android.hospitalapp_arbellayglassey;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class PatientModify extends AppCompatActivity {
+
+
+    private Button btnModifyPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_modify);
+        pressBtnModifyPatient();
+    }
+
+
+
+   // When the user decide to modify the data of a patient
+    public void pressBtnModifyPatient(){
+
+        //Find the id view for the button modify the data of a patient
+        btnModifyPatient = (Button) findViewById(R.id.btn_ok_modify_patient);
+
+        //Add a listener to modify the data of a patient
+        btnModifyPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PatientModify.this, PatientDetails.class);
+                PatientModify.this.startActivity(intent);
+            }
+        });
+
     }
 }
+
