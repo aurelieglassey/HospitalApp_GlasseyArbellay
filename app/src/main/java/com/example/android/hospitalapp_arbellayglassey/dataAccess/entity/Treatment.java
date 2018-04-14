e322da989024da9d23e3857903c6a5f7fd1d5e8d
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-04-14 12:01:41 +0200

    Ré ogranisation des packages

---
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/dataAccess/entity/Treatment.java
@@ -0,0 +1,26 @@
+package com.example.android.hospitalapp_arbellayglassey.dataAccess.entity;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+import static android.arch.persistence.room.ForeignKey.CASCADE;
+
+@Entity (tableName = "Treatment",
+        foreignKeys = @ForeignKey(entity = Patient.class,
+                parentColumns = "id",
+                childColumns = "idPatient",
+                onDelete = CASCADE))
+
+public class Treatment {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+
+}
