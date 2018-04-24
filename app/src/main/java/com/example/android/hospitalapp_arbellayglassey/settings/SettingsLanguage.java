70ae5f0512d1c060f2ede50530113b30b438a855
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-04-24 19:44:35 +0200

    La langue marche presk

--- app/src/main/java/com/example/android/hospitalapp_arbellayglassey/settings/SettingsLanguage.java
+++ app/src/main/java/com/example/android/hospitalapp_arbellayglassey/settings/SettingsLanguage.java
@@ -1,17 +1,26 @@
 package com.example.android.hospitalapp_arbellayglassey.settings;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.android.hospitalapp_arbellayglassey.R;
 
-public class SettingsLanguage extends AppCompatActivity {
+import java.util.Locale;
+
+public class SettingsLanguage extends AppCompatActivity implements View.OnClickListener {
 
     private String titleLanguage = "";
+    private Locale myLocale;
+    private Button btnEnglish, btnFrench;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +28,68 @@ public class SettingsLanguage extends AppCompatActivity {
         setContentView(R.layout.activity_settings_language);
 
         titleLanguage = this.getString(R.string.title_settings_language);
+
+        this.btnEnglish = (Button) findViewById(R.id.btnSettingsLanguageEnglish);
+        this.btnFrench = (Button) findViewById(R.id.btnSettingsLanguageFrench);
+
+        this.btnEnglish.setOnClickListener(this);
+        this.btnFrench.setOnClickListener(this);
+
+
+        loadLocale();
+
     }
 
 
+    public void changeLang(String lang)
+    {
+        if (lang.equalsIgnoreCase(""))
+            return;
+        myLocale = new Locale(lang);
+        saveLocale(lang);
+        Locale.setDefault(myLocale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = myLocale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
 
+    }
+
+    public void saveLocale(String lang)
+    {
+        String langPref = "Language";
+        SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(langPref, lang);
+        editor.commit();
+    }
+
+    public void loadLocale()
+    {
+        String langPref = "Language";
+        SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
+        String language = prefs.getString(langPref, "");
+        changeLang(language);
+    }
+
+    public void onClick(View v) {
+        String lang = "en";
+        switch (v.getId()) {
+            case R.id.btnSettingsLanguageEnglish:
+                lang = "en";
+                Toast.makeText(SettingsLanguage.this, " Language : English ", Toast.LENGTH_LONG).show();
+                finish();
+
+            case R.id.btnSettingsLanguageFrench:
+                lang = "fr";
+                Toast.makeText(SettingsLanguage.this, " Langue : Français ", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+            default:
+                break;
+        }
+        changeLang(lang);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
